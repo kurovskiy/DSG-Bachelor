5c26e78f90c7d6ef2410afae1f85f657f258f867
Alexey Kurovskiy <alexei@kurowski.ru>
2016-05-15 11:14:03 +0200

    Packages and Classes created

---
+++ src/Controller/Downloader.java
@@ -0,0 +1,7 @@
+package Controller;
+
+/**
+ * Created by alexe on 15.05.2016.
+ */
+public class Downloader {
+}
---
+++ src/Servlets/BrowseServlet.java
@@ -0,0 +1,7 @@
+package Servlets;
+
+/**
+ * Created by alexe on 15.05.2016.
+ */
+public class BrowseServlet {
+}
